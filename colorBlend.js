afec4ca917dc9180ffed208d23b36d275c6bdcfd
Tony <un_razum@mail.ru>
2021-08-08 08:34:24 +0300

    Update colorBlend.js

--- colorBlend.js
+++ colorBlend.js
@@ -1,11 +1,7 @@
 const gElems = document.querySelectorAll(".blend");
-
-console.log(gElems);
-
 function change() {
   console.log(gElems);
   for (gElem of gElems) {
-    console.log(gElem);
     gElem.classList.toggle("blend");
   }
 }
